fef9646adb1636b20a379b18877381edde4e6192
V-Emanuel <gvictor567@gmail.com>
2022-11-26 21:26:40 -0300

    palavra aleatória é selecionada quando o botão é clicado

--- src/components /Jogo.js
+++ src/components /Jogo.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import forca0 from "../assets/forca0.png"
 import forca1 from "../assets/forca1.png"
 import forca2 from "../assets/forca2.png"
@@ -5,20 +6,28 @@ import forca3 from "../assets/forca3.png"
 import forca4 from "../assets/forca4.png"
 import forca5 from "../assets/forca5.png"
 import forca6 from "../assets/forca6.png"
+import palavras from "../palavras"
+let img = forca0;
 
 export default function Jogo(){
+
+  let [word, setWord] = useState("");
+
+  function chooseWord(){
+    setWord(palavras[Math.floor(Math.random() * palavras.length)]);
+  }
+
     return (
         <div className='upside'>
         <div className='images'>
-          <img className='forca forca0' src={forca0}></img>
-          <img className='forca forca1' src={forca1}></img>
-          <img className='forca forca2' src={forca2}></img>
-          <img className='forca forca3' src={forca3}></img>
-          <img className='forca forca4' src={forca4}></img>
-          <img className='forca forca5' src={forca5}></img>
-          <img className='forca forca6' src={forca6}></img>
+          <img className='forca forca0' src={img}></img>
         </div>
-        <button className='choose'>Escolher Palavra</button>
+        <div className="direita">
+        <button className='choose' onClick={chooseWord}>Escolher Palavra</button>
+        <p className="randomWord">{word}</p>
+        </div>
+        
+
   </div>
     );
 }
