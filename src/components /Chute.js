98df10e3f461d6b2203321b93088f784526e9668
V-Emanuel <gvictor567@gmail.com>
2022-11-27 20:43:41 -0300

    letras selecionadas mudam de cor

--- src/components /Chute.js
+++ src/components /Chute.js
@@ -1,9 +1,16 @@
+import React from "react";
 export default function Chute(){
+    const [tent, setTent] = React.useState("");
+    function tentativa(){
+        console.log(tent);
+        setTent("");
+    }
     return (
         <div className="chute">
                 <p>Já sei a palavra!</p>
-                <input type="text"/>
-                <button className="chutar">Chutar</button>
+                <input type="text" value={tent} onChange={ev => setTent(ev.target.value)}/>
+                <button className="chutar" onClick={tentativa}>Chutar</button>
+                <p>{tent}</p>
             </div>
     );
 }
--- src/components /Letras.js
+++ src/components /Letras.js
@@ -1,9 +1,17 @@
+import React from "react";
 export default function Letras() {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-        "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const [selectedLetters, setSelectedLetters] = React.useState([])
+    function letterClicked(letra) {
+        setSelectedLetters([...selectedLetters, letra]);
+    }
     function Letter(props) {
         return (
-            <li>{props.letter}</li>
+            <li onClick={() => letterClicked(props.letter)} 
+            className={`${selectedLetters.includes(props.letter) ? "selected" : "nSelected"} `}>
+                {props.letter}
+            </li>
         );
     }
     return (
